42e435a7582f97816f90e1e2f31645f9b5cc341e
aoi-stella <aoi.stella.dev@gmail.com>
2025-02-12 01:58:00 +0900

    rename view name

---
+++ Pencula/Pencula/Modules/SignUp/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Pencula
+//
+//  Created by はると on 2025/02/12.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
