06bfae0921bcc7e14abde0ef1cbd4c9efd316845
aoi-stella <aoi.stella.dev@gmail.com>
2025-02-11 20:46:19 +0900

    setup initially project

---
+++ Pencula/Pencula/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pencula
+//
+//  Created by はると on 2025/02/09.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Pencula/PenculaTests/PenculaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PenculaTests.swift
+//  PenculaTests
+//
+//  Created by はると on 2025/02/09.
+//
+
+import Testing
+@testable import Pencula
+
+struct PenculaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
