3ba0b088bacf63aa391068da8797b06314e1eb0f
aoi-stella <aoi.stella.dev@gmail.com>
2025-02-24 12:49:51 +0900

    add title of signup screen

--- Pencula/Pencula/Modules/SignUp/SignUpView.swift
+++ Pencula/Pencula/Modules/SignUp/SignUpView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct SignUpView: View {
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [
+            .font: UIFont.preferredFont(forTextStyle: .largeTitle)
+        ]
+        UINavigationBar.appearance().standardAppearance = appearance
+    }
     var body: some View {
-        VStack {
-            Text("Hi! Welcome to Pencula!!")
-                .background(Color.positive)
+        NavigationView {
+            VStack {
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.screenBackground)
+            .navigationTitle("会員登録")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.background)
+        .padding(Spacing.md)
+        .background(Color.screenBackground)
     }
 }
 
